1c027819da077d2b2aa98405f09cae246c05ec6d
YanBin <nyb123456nyb@163.com>
2020-04-23 10:24:36 +0800

    move es query function to elasitc_query.py and use log_decorator module to log

--- nhids/main.py
+++ nhids/main.py
@@ -3,67 +3,21 @@
 
 import os
 import sys
-from datetime import datetime
 
 #Third-party libraries
-from elasticsearch import Elasticsearch,helpers
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 #self own libraries
 NIDS_BASE_DIR = \
 os.path.dirname(os.path.dirname(os.path.abspath("__file__")))
 sys.path.append(NIDS_BASE_DIR)
-from log import *
 from data_process import DataProcess
 from conf import setting
-
-es = Elasticsearch(['http://10.212.18.29:9200','http://10.212.21.60:9200'],http_auth=('elastic','abc123!@#'),
-                   sniff_on_start=True,sniff_on_connection_fail=True,sniff_timeout=5)
-
-def query_data(date):
-    gte = "now-"+str(date)+"m"
-    query_data = {
-         "query": {
-            "range": {
-                "@timestamp": {
-                    "gte": gte,
-                    "lte": "now",
-                    "format": "epoch_millis"
-                }
-            }
-        }
-    }
-    #query_data = {
-    #     "query": {
-    #           "bool": {
-    #               "must": [
-    #                   {
-    #                    "range": {
-    #                        "timestamp": {
-    #                            "gte": gte,
-    #                            "lte": "now",
-    #                            "format": "epoch_millis"
-    #                        }
-    #                    }
-    #                }
-    #            ]
-    #        }
-    #    }
-    #}
-    return query_data
-
-def scroll_search(index='wazuh-alerts-3.x-*',doc_type="wazuh"):
-    result = []
-    scanResp = helpers.scan(es, query=query_data(setting.INTERVAL),doc_type=doc_type,
-                            index=index, scroll= "1m")
-    for hit in scanResp:
-        result.append(hit)
-    log.info("%s result is: %s"%(index,len(result)))
-    return result
+from elastic_query import ScrollSearch
 
 def process():
-    hids_result = scroll_search(index="wazuh-alerts-3.x-*",doc_type="wazuh")
-    nids_result = scroll_search(index="filebeat_suricata-*",doc_type="doc")
+    hids_result = ScrollSearch(index="wazuh-alerts-3.x-*",doc_type="wazuh")
+    nids_result = ScrollSearch(index="filebeat_suricata-*",doc_type="doc")
     DataProcess(hids_result,nids_result)
 
 if __name__ == "__main__":
