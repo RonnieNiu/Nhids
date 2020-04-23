43f9e0b80a9bba8da0cfc195357a556b671e9116
YanBin <nyb123456nyb@163.com>
2020-04-23 10:31:20 +0800

    add log_decorator module

--- nhids/log.py
+++ nhids/log.py
@@ -5,6 +5,8 @@ import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
 
+from functools import wraps
+
 from conf import setting
 
 log_path = setting.LOGPATH
@@ -16,7 +18,7 @@ if not os.path.exists(log_path):
 
 log = logging.getLogger('ybn')
 log.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s: %(filename)s[第%(lineno)d行]: %(levelname)s:%(message)s")
+formatter = logging.Formatter("%(asctime)s: %(levelname)s:%(message)s")
 log_file_handler = \
         TimedRotatingFileHandler(filename=os.path.join(log_path,"run"),when="MIDNIGHT",interval=1)
 log_file_handler.setFormatter(formatter)
@@ -24,3 +26,17 @@ log_file_handler.setLevel(logging.DEBUG)
 log_file_handler.suffix = "%Y-%m-%d.log"
 log.addHandler(log_file_handler)
 
+
+def log_decorator(func):
+    @wraps(func)
+    def wrapper(*args,**kv):
+        result = func(*args,**kv)
+        if isinstance(result,Exception) == True:
+            log.error("%s: %s"%(func.__name__,result))
+        elif func.__name__ == "ScrollSearch":
+            if kv.get("index") != None:
+                log.info("%s: %s result is: %s"%(func.__name__,kv["index"],len(result)))
+        elif func.__name__ == "DataProcess":
+            log.info("%s: the nhids num is: %d\n"%(func.__name__,len(result)))
+        return result
+    return wrapper
